0a9bd3d73611103e48fca79c366870b1dcad6ba7
Feroze Shah <fshah@mit.edu>
2018-04-28 16:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,15 +21,12 @@ function sayHiToGrandma(string) {
   var uppercase = string
   var lowercase = string
   if(uppercase.toUpperCase() === string){
-    console.log("YES INDEED!")
     return "YES INDEED!"
   }
   else if(lowercase.toLowerCase() === string){
-    console.log("I can't hear you!")
     return "I can't hear you!"
   }
   else if(string === "I love you, Grandma."){
-    console.log("I love you, too.")
     return "I love you, too."
   }
 }
