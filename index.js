6b199dcf5da8e3cd9420722ec94d8fb1b22e8f76
Feroze Shah <fshah@mit.edu>
2018-04-28 16:49:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function logWhisper(string) {
   return string.toLowerCase()
 }
 
+function sayHiToGrandma(string) {
+  if(string.toUpperCase === string){
+    return "YES INDEED!"
+  }
+  else if(string.toLowerCase === string){
+    return "I can't hear you!"
+  }
+  else if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
